e5feca48a1d213213bf253623cee2004963a0711
Sandeshdahal10 <sandeshdahal3547@gmail.com>
2025-07-15 16:45:42 +0545

    done

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
   try{
-    const connect = await mongoose.connect('mongodb+srv://sandesh:sandesh123@assignmentcluster.anpuejz.mongodb.net/?retryWrites=true&w=majority&appName=Assignmentcluster');
+    const connect = await mongoose.connect;
     console.log('MongoDB connected successfully!');
   }catch(err){
     console.error('MongoDB connection failed:', err);
